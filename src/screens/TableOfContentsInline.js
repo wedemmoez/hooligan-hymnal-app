f10fb15e4d85332c79ec3f478dbb510f72f8aaf7
Sam Shrum <Sam.L.Shrum@gmail.com>
2019-09-05 19:51:49 -0400

    Numbers are broken in iOS, kill for now

--- src/screens/TableOfContentsInline.js
+++ src/screens/TableOfContentsInline.js
@@ -67,9 +67,6 @@ class SongRow extends React.Component {
               <RegularText style={{marginRight: 10}}>
                 {capoSignal}
               </RegularText>
-              <RegularText style={styles.pageLabel}>
-                {song.toc_page_label}
-              </RegularText>
             </View>
           </View>
         </View>
